7019b4079d15d15b37861b5fc84fdb4b121f75d9
AndrewDTN <xxxxx@gmail.com>
2020-12-27 21:49:10 +0800

    2fin

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Game
 ## 第一階段
-![image](/image/1-1.jpg)
+![image](/image/1-1.png)
 ![image](/image/1-2.png)
 ![image](/image/1-3.png)
 ![image](/image/1-4.png)
+## 第二階段
+![image](/image/2-0.png)
+![image](/image/2-1.png)
+![image](/image/2-2.png)
+![image](/image/2-3.png)
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var main ={
     preload: function(){
-        game.load.image('sky','assets/sky.png')
-        game.load.image('ground','assets/platform.png')
+        game.load.image('sky','assets/sky.png');
+        game.load.image('ground','assets/platform.png');
+        //player
+        game.load.spritesheet('dude','assets/dude.png',32,48)
     },
     
     create: function(){
@@ -18,13 +20,44 @@ var main ={
         this.platforms.create(-150, 250, 'ground');
   
         this.platforms.setAll('body.immovable', true);
+        
+        //player
+        this.player = game.add.sprite(32,game.world.height.height-150,'dude')
+        game.physics.arcade.enable(this.player)
+        this.player.body.bounce.y = 0.2
+        this.player.body.gravity.y = 300
+        this.player.body.collideWorldBounds = true
+        this.player.body.setSize(20,32,5,16)
+        
+        this.player.animations.add('left',[0,1,2,3],10,true)
+        this.player.animations.add('right',[5,6,7,8],10,true)
+        
+        this.cursors = game.input.keyboard.createCursorKeys()
      },
     
     update: function(){
-    
+       game.physics.arcade.collide(this.player,this.platforms)
+        
+        this.player.body.velocity.x = 0
+        if(this.cursors.left.isDown){
+            this.player.body.velocity.x = -150
+            this.player.animations.play('left')
+        }
+        else if (this.cursors.right.isDown){
+            this.player.body.velocity.x = 150
+            this.player.animations.play('right')
+        }
+        else {
+            this.player.animations.stop()
+            this.player.frame = 4
+        }
+        
+        if(this.cursors.up.isDown && this.player.body.touching.down){
+            this.player.body.velocity.y = -350
+        }
     },
 }
 
-var game = new Phaser.Game( 800,600, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(800,600, Phaser.AUTO, 'gameDiv');
 game.state.add('main', main);
 game.state.start('main');
